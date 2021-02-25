8c739165c80cb52a5f105f679405c5dc0ea62d2b
Sven Handt <sven.handt@kps.com>
2021-02-25 23:33:10 +0100

    Entwicklung

--- src/main/java/com/svenhandt/app/cinemaapp/service/CommonPresentationService.java
+++ src/main/java/com/svenhandt/app/cinemaapp/service/CommonPresentationService.java
@@ -1,7 +1,6 @@
 package com.svenhandt.app.cinemaapp.service;
 
 import com.svenhandt.app.cinemaapp.entity.Presentation;
-import com.svenhandt.app.cinemaapp.view.BookingView;
 
 
 public interface CommonPresentationService
